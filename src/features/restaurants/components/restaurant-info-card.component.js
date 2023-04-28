f13a423fc686133b55d1050012bf84d29d228c88
Karthikeyan N <109551096+Karthikoffl@users.noreply.github.com>
2023-04-28 18:26:45 +0530

    s7 - 130 commit

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,37 +1,57 @@
 import React from 'react';
-import styled from 'styled-components/native';
-import { Text } from 'react-native';
-import { Card } from 'react-native-paper';
-
-const Title = styled.Text`
-    padding: ${(props) => props.theme.space[3]};
-    color: ${(props) => props.theme.colors.ui.primary};
-`;
-
-const RestaurantCard = styled(Card)`
-    background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-// use external component with styled component by using (external component)
-const RestaurantCardCover = styled(Card.Cover)` 
-    padding: ${(props) => props.theme.space[3]};
-    background-color: ${(props) => props.theme.colors.bg.primary};
-`;
+import { SvgXml } from 'react-native-svg';
+import star from '../../../../assets/star';
+import open from '../../../../assets/open';
+import { Spacer } from '../../../components/spacer/spacer.component';
+import { Text } from '../../../components/typography/text.component';
+import { 
+    Icon, 
+    Address, 
+    RestaurantCard, 
+    RestaurantCardCover, 
+    Info, 
+    Section, 
+    Rating, 
+    SectionEnd 
+} from './restaurant-info-card.styles';
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
     const {
-        name = "Restaurant Name",
-        icon, 
+        name = "Lazeez Kitchen",
+        icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png", 
         photos = ["https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg"], 
         address = "Chennai, India", 
         isOpenNow = true, 
         rating = 4, 
-        isClosedTemporarily, 
+        isClosedTemporarily = true, 
     } = restaurant;
+
+    const ratingArray = Array.from(new Array(Math.floor(rating)));
     return (
         <RestaurantCard elevation={5}>
             <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-            <Title>{name}</Title>
+            <Info>
+                <Text variant="label">{name}</Text>
+                <Section>
+                <Rating>
+                {ratingArray.map(() => (
+                    <SvgXml xml={star} width={20} height={20} />
+                ))}
+                </Rating>
+                <SectionEnd>
+                    {isClosedTemporarily && (
+                        <Text variant="error">CLOSED TEMPORARILY</Text>
+                    )}
+                    <Spacer position="left" size="large">
+                        {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+                    </Spacer>
+                    <Spacer position="left" size="large">
+                        <Icon source={{ uri: icon }} />
+                    </Spacer>
+                </SectionEnd>
+                </Section>
+                <Address>{address}</Address>
+            </Info>    
         </RestaurantCard>
     );
 };
