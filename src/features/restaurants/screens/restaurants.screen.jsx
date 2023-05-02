03fa09c30a61ed1642a8ffe41f472402ed86ebe9
Karthikeyan N <109551096+Karthikoffl@users.noreply.github.com>
2023-05-02 15:58:43 +0530

    s8 - 150

--- src/features/restaurants/screens/restaurants.screen.jsx
+++ src/features/restaurants/screens/restaurants.screen.jsx
@@ -1,37 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { FlatList } from 'react-native';
-import { Searchbar } from 'react-native-paper';
+import { Searchbar, MD2Colors } from 'react-native-paper';
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
-import { SearchContainer } from './restaurants.screen.styles';
+import { SearchContainer, LoadingContainer, Loading } from './restaurants.screen.styles';
 import { Spacer } from '../../../components/spacer/spacer.component';
 import { SafeArea } from '../../../components/utility/safe-area.component';
+import { RestaurantsContext } from '../../../services/restaurants/restaurants.context';
 
 
-export const RestaurantsScreen = () => (
+
+export const RestaurantsScreen = () => {
+  const { isLoading, error, restaurants } = useContext(RestaurantsContext);
+  return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={MD2Colors.red800} />
+        </LoadingContainer>
+      )}
       <SearchContainer>       
         <Searchbar />
       </SearchContainer>
       <FlatList 
-        data={[
-          {name: 1}, 
-          {name: 2}, 
-          {name: 3}, 
-          {name: 4}, 
-          {name: 5}, 
-          {name: 6}, 
-          {name: 7}, 
-          {name: 8},
-          {name: 9},
-          {name:10},
-        ]}
-        renderItem={() =>
+        data={restaurants}
+        renderItem={({ item }) => {
+          return (
         <Spacer position='bottom' size='large'>
-          <RestaurantInfoCard />
+          <RestaurantInfoCard restaurant={item}/>
         </Spacer>
-        }
+        );}}
         keyExtractor={(item) => item.name}
         contentContainerStyle={{padding: 16}}
       />
     </SafeArea>
-);
+)};
--- src/features/restaurants/screens/restaurants.screen.styles.js
+++ src/features/restaurants/screens/restaurants.screen.styles.js
@@ -1,9 +1,19 @@
 import styled from 'styled-components/native';
+import { ActivityIndicator } from 'react-native-paper';
 
 export const SearchContainer = styled.View`
     padding: ${(props) => props.theme.space[3]};
 `;
 
+export const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+
+export const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
 // export const RestaurantList = styled(FlatList).attrs({
 //     contentContainerStyle: {
 //         padding: 16,
